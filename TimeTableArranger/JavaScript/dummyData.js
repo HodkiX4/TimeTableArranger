0f3051729f403091ebacc6309d6d8a8648121c82
AronX4 <loptok3@gmail.com>
2025-10-21 20:30:46 +0200

    feat: Implemented basic htmlstructure  with some styles using BootStrap, and implemented drag and drop functionality

---
+++ TimeTableArranger/JavaScript/dummyData.js
@@ -0,0 +1,14 @@
+export const subjects = [
+    "Alkalmazásfejlesztés II.",
+    "Mesterséges Inteligencia I",
+    "Szoftvertesztelés",
+    "Progresszív Webalkalmazás fejlesztés",
+];
+
+export const weekDays = [
+    "Hétfő",
+    "Kedd",
+    "Szerda",
+    "Csütörtök",
+    "Péntek"
+];
---
+++ TimeTableArranger/JavaScript/index.js
@@ -0,0 +1,132 @@
+import { subjects, weekDays } from "./dummyData.js";
+
+// Fills and unordered list with list items containing subjects.
+function createSubjectTable(table) {
+    const tableBody = table.lastElementChild;
+    subjects.map((s, i) => {
+        const tableRow = document.createElement('tr');
+        const tableData = document.createElement('td');
+        tableData.setAttribute("isDeletable", "true");
+        tableData.textContent = s;
+        tableData.draggable = true;
+        tableRow.appendChild(tableData);
+        tableBody.appendChild(tableRow)
+    });
+}
+
+// Creates the content of the timetable
+function createTimeTable(table) {
+    const tableHead = table.firstElementChild;
+    const tableBody = table.lastElementChild;
+    createTableHead(tableHead);
+    createTableBody(tableBody);
+ 
+}
+
+// Creates the head of the timetable
+function createTableHead(head) {
+    const headerRow = document.createElement('tr');
+    
+    headerRow.innerHTML = "<th style='width: 2rem;'>Idő</th>";
+    head.appendChild(headerRow);
+    weekDays.map(d => {
+        headerRow
+        .innerHTML += `
+            <th style='width: 20%'>${d}</th>
+            `
+    })
+}
+
+// Creates the body of the timetable
+function createTableBody(body) {
+    // This for loop creates the table rows from 8 to 16 O'clock 
+   for(let i = 8; i <= 16; i++) {
+        const tableRow = document.createElement('tr');
+        // This for loop feels each row with a "td" tag for each day we have in the timetable.
+        for(let j = 0; j <= weekDays.length; j++) {
+            const tableData = document.createElement('td');
+            if(j === 0) {
+                tableData.textContent = `${i}:00`;
+                tableData.classList.add("align-top");
+            } else {
+                tableData.draggable = true;
+            }
+            tableRow.appendChild(tableData);
+                
+        }
+        body.appendChild(tableRow);
+    }
+}
+
+// This makes sure that the code runs only after the DOM has loaded, so we don't try to accidentally access an element that doesn't exist yet, but it's not needed, if we place the script at the end of the body tag, I just put it here to make sure everything works fine.
+window.document.addEventListener("DOMContentLoaded", () => {
+    const subjectTable = document.getElementById("subjects-table");
+    const timeTable = document.getElementById("time-table");
+    createSubjectTable(subjectTable);
+    createTimeTable(timeTable);
+
+    const subjectTableBody = subjectTable.lastElementChild;
+    const timeTableBody = timeTable.lastElementChild;
+    if(subjectTableBody === null || timeTableBody === null) {
+        return;
+    }
+
+    let dragElement = null;
+
+    for(let row of timeTableBody.children) {
+        for(let data of row.children) {
+            data.addEventListener("dragover", (e) => {
+                e.preventDefault();
+            });
+
+            data.addEventListener("drop", (e) => {
+                if(e.target.innerText === "") {
+                    e.target.innerText = dragElement.innerText;
+                    if(dragElement.hasAttribute("isDeletable")) {
+                        const dragElementParent = dragElement.parentElement;
+                        subjectTableBody.removeChild(dragElementParent);
+                        
+                    } else {
+                        dragElement.innerText = "";
+                    }
+                } else {
+                    // TODO -> Modal pop up needed for implementing user choice
+                    let swapperElement = dragElement.innerText;
+                    dragElement.innerText = e.target.innerText;
+                    e.target.innerText = swapperElement;
+                }
+
+                dragElement.removeAttribute("isDeletable");
+            });
+
+            data.addEventListener("dragstart", (e) => {
+                dragElement = e.currentTarget;
+            }) ;
+        }
+    }
+
+    for(let row of subjectTableBody.children) {
+        const data = row.firstElementChild;               
+        data.addEventListener("dragstart", (e) => {
+            dragElement = e.currentTarget;
+        });
+        data.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            console.log("macska");
+            
+        });
+        data.addEventListener("drop", (e) => {
+            e.preventDefault();
+
+            const tableRow = document.createElement('tr');
+            const tableData = document.createElement('td');
+            tableData.draggable = true;
+            tableData.innerText = dragElement.innerText;
+            tableRow.appendChild(tableData)
+            subjectTableBody.appendChild(tableRow);
+            if(!dragElement.hasAttribute("isDeletable")) {
+                dragElement.innerText = "";
+            }
+        });
+    }
+});
